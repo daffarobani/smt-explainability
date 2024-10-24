f9dc123c98f0ac6cd468c2ec16e38afcc35c51c4
Paul-Saves <paul.saves@alumni.enac.fr>
2024-10-24 12:27:08 +0200

    ruff

--- smt_ex/pdp/pd_interaction_display.py
+++ smt_ex/pdp/pd_interaction_display.py
@@ -125,6 +125,4 @@ class PDFeatureInteractionDisplay:
         ax.set_axisbelow(True)
         fig.tight_layout()
 
-        # Close the figure before returning
-        plt.close(fig)
         return fig
