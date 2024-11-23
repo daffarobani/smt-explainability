6ad20fc0d4376e65883fcfeb52371ebd179f6705
Muhammad Daffa Robani <robani.daffa@gmail.com>
2024-11-23 22:34:25 +0700

    add docs

--- smt_explainability/shap/shap_feature_importance.py
+++ smt_explainability/shap/shap_feature_importance.py
@@ -3,8 +3,26 @@ import numpy as np
 
 
 def compute_shap_feature_importance(
-    instances, model, x, is_categorical, *, method="kernel"
+    instances,
+    model,
+    x,
+    is_categorical,
+    *,
+    method="kernel"
 ):
+    """
+    Computes feature importance based on SHAP values on the given instances.
+
+    Args:
+        instances (numpy.ndarray): Instances for which SHAP values are computed.
+        model (object): The model used for predictions.
+        x (numpy.ndarray): Reference data used for SHAP value computation.
+        is_categorical (list): List indicating whether each feature is categorical.
+        method (str, optional): Method to use for SHAP value computation ('kernel' or 'exact').
+
+    Returns:
+        numpy.ndarray: Computed SHAP feature importances.
+    """
     shap_values = compute_shap_values(
         model,
         instances,
